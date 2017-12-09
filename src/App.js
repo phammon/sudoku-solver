0700e4c26fb3aa65a342a05ab67f67e5c27eb159
Patrick Hammon <phammon@Patricks-MBP.home>
2017-12-09 15:32:45 -0500

    added getter functions

--- src/App.js
+++ src/App.js
@@ -6,87 +6,80 @@ import './App.css';
 export default class App extends Component {
 sudoku(puzzle) {
 let nums = [1,2,3,4,5,6,7,8,9];
-var newPuzzle = puzzle.map(function (nested) {
-    return nested.map(function (element) {
-        if(element < 1) {
-          
-        }else{
-          return element;
-        }
-    });
-});
-let column0 = puzzle.map(function(value,index) { return value[0]; });
-let column1 = puzzle.map(function(value,index) { return value[1]; });
-let column2 = puzzle.map(function(value,index) { return value[2]; });
-let column3 = puzzle.map(function(value,index) { return value[3]; });
-let column4 = puzzle.map(function(value,index) { return value[4]; });
-let column5 = puzzle.map(function(value,index) { return value[5]; });
-let column6 = puzzle.map(function(value,index) { return value[6]; });
-let column7 = puzzle.map(function(value,index) { return value[7]; });
-let column8 = puzzle.map(function(value,index) { return value[8]; });
 
-//squares are returned from left to right
+//getter functions
+function getRow(rowNum, puzzle) {
+  let row = puzzle[rowNum];
+  return row;
+}
+
+function getColumn(colNum, puzzle) {
+  let col =  puzzle.map(function(value, index) {
+     return value[colNum];
+  });
+   return col;
+}
+//squares are returned from Top to Bottom
 function getSquare(col1, col2, col3, square) {
  let squareArr = []
   if(square === 1) {
    squareArr.push(col1.slice(0, 3)) 
    squareArr.push(col2.slice(0, 3))
    squareArr.push(col3.slice(0, 3));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 2) {
    squareArr.push(col1.slice(3, 6)) 
    squareArr.push(col2.slice(3, 6))
    squareArr.push(col3.slice(3, 6));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 3) {
    squareArr.push(col1.slice(6, 9)) 
    squareArr.push(col2.slice(6, 9))
    squareArr.push(col3.slice(6, 9));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 4) {
    squareArr.push(col1.slice(0, 3)) 
    squareArr.push(col2.slice(0, 3))
    squareArr.push(col3.slice(0, 3));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 5) {
    squareArr.push(col1.slice(3, 6)) 
    squareArr.push(col2.slice(3, 6))
    squareArr.push(col3.slice(3, 6));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 6) {
    squareArr.push(col1.slice(6, 9)) 
    squareArr.push(col2.slice(6, 9))
    squareArr.push(col3.slice(6, 9));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 7) {
    squareArr.squarepush(col1.slice(0, 3)) 
    squareArr.push(col2.slice(0, 3))
    squareArr.push(col3.slice(0, 3));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 8) {
    squareArr.push(col1.slice(3, 6)) 
    squareArr.push(col2.slice(3, 6))
    squareArr.push(col3.slice(3, 6));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }else if(square === 9) {
    squareArr.push(col1.slice(6, 9)) 
    squareArr.push(col2.slice(6, 9))
    squareArr.push(col3.slice(6, 9));
-   return squareArr.join().split(",");
+   squareArr.join().split(",");
  }
+ //convert array of strings to array of ints
+ square = squareArr.map(Number);
+ return square;
 }
-//convert array of strings to array of integers
-let square = getSquare(column0,column1,column2, 1).map(Number);
 
-function fillSquareWithCorrectInts(col, row, square) {
-  let sumOfColumn = col.reduce((total, value) => total + value);
-  let sumOfRow = row.reduce((total, value) => total + value);
-  let sumOfSquare = square.reduce((total,value) => total + value);
+  //let sumOfColumn = getColumn.reduce((total, value) => total + value);
+  //let sumOfSquare = getSquare.reduce((total,value) => total + value);
   function checkCol(col) {
     if(sumOfColumn !== 45) {
       //see missing numbers and make a guess
+     }
     }
-  }
   function checkRow(row) {
     if(sumOfRow !== 45) {
       //see missing numbers and make a guess
@@ -97,7 +90,6 @@ function fillSquareWithCorrectInts(col, row, square) {
       //see missing numbers and make a guess
     }
   }
-}
 }
 
 
